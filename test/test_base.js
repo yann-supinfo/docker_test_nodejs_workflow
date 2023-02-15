4473e971bc5a673a607aabc57824784662c41b84
yann-supinfo <yann1.bastie@supinfo.com>
2023-02-15 13:39:04 +0100

    test2

--- test/test_base.js
+++ test/test_base.js
@@ -2,8 +2,10 @@ var assert = require('chai').assert
   , foo = 'bar'
   , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
 
-assert.typeOf(foo, 'string'); // without optional message
-assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
-assert.equal(foo, 'bar', 'foo equal `bar`');
-assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
-assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
+  describe('Math', function() {
+    describe('#multiply()', function() {
+      it('should multiply two numbers correctly', function() {
+        assert.equal(2*3, 5);
+      });
+    });
+  });
