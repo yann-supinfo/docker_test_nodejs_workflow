e5bfc8f6005ccf13860aeef0679d2c2b285df08c
yann-supinfo <yann1.bastie@supinfo.com>
2023-02-18 15:26:02 +0100

    mysql, sequelize, env 1

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+);
 
 
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get('/', (req, res) => {
   res.send('<h1 style="width:200px;height:200px;background-color:blue;">New Title</h1>');
