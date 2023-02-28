1641cf8df5a364707e51c628df66a8de9cd1099a
Rahuz7 <mercier.julien@protonmail.com>
2023-02-28 19:35:09 +0100

    Removed test of func_test()

--- test/test_base.js
+++ test/test_base.js
@@ -1,34 +1,34 @@
-const func = require('../services/func_test')
+// const func = require('../services/func_test')
 
 
-var assert = require('chai').assert
-  , foo = 'bar'
-  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+// var assert = require('chai').assert
+//   , foo = 'bar'
+//   , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
 
-  describe('Array', function() {
-    describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-        assert.equal([1,2,3].indexOf(4), -1);
-      });
-    });
-  });
+//   describe('Array', function() {
+//     describe('#indexOf()', function() {
+//       it('should return -1 when the value is not present', function() {
+//         assert.equal([1,2,3].indexOf(4), -1);
+//       });
+//     });
+//   });
 
-  describe('test 2', () => {
+//   describe('test 2', () => {
 
-    it('addition de deux nombres est égale à 5', () => {
-         const a = 3;
-         const b = 3;
-         assert.equal(a + b, 6)
-    })
+//     it('addition de deux nombres est égale à 5', () => {
+//          const a = 3;
+//          const b = 3;
+//          assert.equal(a + b, 6)
+//     })
 
-  })
+//   })
 
-  describe('test de la fonction mult', () => {
+//   describe('test de la fonction mult', () => {
 
-    it('multiplication fonctionne', () => {
-         const a = 2;
-         const b = 3;
-         assert.equal(func.mult(a, b), 6)
-    })
+//     it('multiplication fonctionne', () => {
+//          const a = 2;
+//          const b = 3;
+//          assert.equal(func.mult(a, b), 6)
+//     })
 
-  })
+//   })
