d0fbbd79def89708acd73569e148d21fa7820fd6
Rahuz7 <mercier.julien@protonmail.com>
2023-03-01 13:23:01 +0100

    Handled null value and user already exist

--- services/user.service.js
+++ services/user.service.js
@@ -12,26 +12,35 @@ const REGEXP_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d
 // create
 const createUser = async (mail, pwd, lastname, firstname, phone) => {
 
+    if(mail === null) {
+        console.log("DEBUG : ", mail);
+        throw new Error('mail is null');
+    }
     if(mail === "" || typeof mail !== "string") throw new Error('should be a string not empty');
     if(!REGEXP_email.test(mail)) throw new Error('invalid email');
 
+    if(lastname === null) throw new Error('lastname is null');
     if(lastname === "" || typeof lastname !== "string") throw new Error('should be a string not empty');
     if(!REGEXP_name.test(lastname)) throw new Error('invalid lastname');
 
+    if(firstname === null) throw new Error('firstname is null');
     if(firstname === "" || typeof firstname !== "string") throw new Error('should be a string not empty');
     if(!REGEXP_name.test(firstname)) throw new Error('invalid firstname');
 
+    if(phone === null) throw new Error('phone is null');
     if(phone === "" || typeof phone !== "string") throw new Error('should be a string not empty');
     if(!REGEXP_phone.test(phone)) throw new Error('invalid phone');
 
+    if(pwd === null) throw new Error('password is null');
     if(!REGEXP_password.test(pwd)) throw new Error('invalid password. It should contain at least : \n- 1 uppercase\n- 1 lowercase\n- 1 special character\n- 1 number\n- 7 characters');
 
     try {
         const queryInterface = db.sequelize.getQueryInterface();
         const tables = await queryInterface.showAllTables();
-        if (!tables.includes('users')) {
+        if(!tables.includes('users')) {
             throw new Error('users table does not exist');
         }
+        if(await db.user.findOne({ where: {email: mail} }) !== null || await db.user.findOne({ where: {telephone: phone} }) !== null) throw new Error('user already exist');
         const User = await db.user.create({
             email: mail,
             password: bcrypt.hashSync(pwd, 8),
