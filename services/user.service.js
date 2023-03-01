866140a10b34adb81cf4f319d86994c4dc15ac14
Rahuz7 <mercier.julien@protonmail.com>
2023-03-01 11:57:53 +0100

    Added passwordEncryption and checkTableExist

--- services/user.service.js
+++ services/user.service.js
@@ -1,9 +1,13 @@
 const db = require('../models');
+const { Sequelize } = require('sequelize');
+const bcrypt = require("bcryptjs");
+
 
 
 const REGEXP_email = /^\S+@\S+\.\S+$/;
 const REGEXP_name = /^[a-zA-Z]+$/;
 const REGEXP_phone = /^(0|\+33)[-. ]?[1-9]{1}([-. ]?[0-9]{2}){4}$/;
+const REGEXP_password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$/;
 
 // create
 const createUser = async (mail, pwd, lastname, firstname, phone) => {
@@ -20,17 +24,24 @@ const createUser = async (mail, pwd, lastname, firstname, phone) => {
     if(phone === "" || typeof phone !== "string") throw new Error('should be a string not empty');
     if(!REGEXP_phone.test(phone)) throw new Error('invalid phone');
 
+    if(!REGEXP_password.test(pwd)) throw new Error('invalid password. It should contain at least : \n- 1 uppercase\n- 1 lowercase\n- 1 special character\n- 1 number\n- 7 characters');
+
     try {
+        const queryInterface = db.sequelize.getQueryInterface();
+        const tables = await queryInterface.showAllTables();
+        if (!tables.includes('users')) {
+            throw new Error('users table does not exist');
+        }
         const User = await db.user.create({
             email: mail,
-            password: pwd,
+            password: bcrypt.hashSync(pwd, 8),
             nom: lastname,
             prenom: firstname,
             telephone: phone, 
         });
-        console.log('User ${User.nom} ${User.prenom} has been successfully added')
+        console.log(`User ${User.nom} ${User.prenom} has been successfully added`);
     } catch (err) {
-        console.log('Unable to connect to the database : ', err);
+        console.log(err);
     }
 
 }
